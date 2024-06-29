3b26bd5265d12eacdab5319a104a62939fd62589
SAGAR ROY <111957606+SagaRoy12@users.noreply.github.com>
2024-06-29 13:13:45 -0700

    Create arrowFunction.js
    a new file contained ARROW FUNCTION added

---
+++ arrowFunction.js
@@ -0,0 +1,14 @@
+
+// Single line of code
+//let add =  (a, b) => a + b;
+    
+
+const add1 =  (a, b)=> {
+	const c = (a+b)
+    console.log("the result is " + c)
+    return (a + b);
+}
+
+// Calling function
+console.log(add1(2, 3))
+
